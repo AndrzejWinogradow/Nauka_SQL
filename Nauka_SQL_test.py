d5c2fcb9410de231322bc0f1717556701118b25c
Andrzej_Winogradow <andrzej.winogradow@gmail.com>
2021-08-04 16:51:55 +0200

    add test.py

---
+++ Nauka_SQL_test.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# import sqlite3
+
+from sqlitedict import SqliteDict
+mydict = SqliteDict('./my_db.sqlite', autocommit=True)
+mydict['some_key'] = any_picklable_object
+print(mydict['some_key'])  # prints the new value
+for key, value in mydict.iteritems():
+    print(key, value)
+print(len(mydict)) # etc... all dict functions work
+mydict.close()
